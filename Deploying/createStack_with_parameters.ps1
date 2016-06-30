aee44e14e248dc0920705395ae78da59ad4fce6e
ENGELSEN Brynjar <brynjar.engelsen@soprasteria.com>
2016-06-30 16:05:34 +0200

    fixed a mistake

--- Deploying/createStack_with_parameters.ps1
+++ Deploying/createStack_with_parameters.ps1
@@ -1,9 +1,14 @@
-﻿Import-Module AWSPowerShell
+﻿
+
+Import-Module AWSPowerShell
 Import-Module AwsCfn
 
 #Set-AWSCredentials -AccessKey AKIAIOSFODNN7EXAMPLE -SecretKey wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY -StoreAs MyProfileName
 #Initialize-AWSDefaults -ProfileName MyProfileName -Region us-west-1
 
 $parameter_file = Get-Content "D:\Users\bengelse\Documents\Amazone\AWS_Cloud_Formations\parameters.json" | ConvertFrom-Json
+$template = Get-Content "D:\Users\bengelse\Documents\Amazone\AWS_Cloud_Formations\test.json"
 
-New-CFNStack -StackName myStack -TemplateBody "$stack" -Parameters $parameters
+$parameters = @($parameter_file)
+
+New-CFNStack -StackName myStack -TemplateBody "$template" -Parameters $parameters

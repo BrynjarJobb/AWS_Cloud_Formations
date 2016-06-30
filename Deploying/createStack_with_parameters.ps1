06eef7a1d0c4860430bb7d85f2582d40c5d5be70
ENGELSEN Brynjar <brynjar.engelsen@soprasteria.com>
2016-06-30 15:38:10 +0200

    Made the powershell-script much easier

--- Deploying/createStack_with_parameters.ps1
+++ Deploying/createStack_with_parameters.ps1
@@ -6,18 +6,4 @@ Import-Module AwsCfn
 
 $parameter_file = Get-Content "D:\Users\bengelse\Documents\Amazone\AWS_Cloud_Formations\parameters.json" | ConvertFrom-Json
 
-$parameters = @()
-foreach ($parameter in $parameter_file)
-{
-    $parameter_object= new-object Amazon.CloudFormation.Model.Parameter
-    
-    $parameter_object.ParameterKey = $parameter.ParameterKey
-    $parameter_object.ParameterValue = $parameter.ParameterValue
-
-    $parameters+= @($parameter_object)
-
-
-}
-
-
 New-CFNStack -StackName myStack -TemplateBody "$stack" -Parameters $parameters
